f9a98d9151891c6704570c133f8e3b665e33b19c
Julek05 <37105470+Julek05@users.noreply.github.com>
2019-05-22 22:17:14 +0200

    Add files via upload

---
+++ poprzednie_wersje+logowanie/inserty/db_attributes_addMandatoryAttribute.php
@@ -0,0 +1,105 @@
+﻿<!DOCTYPE html>
+<head>
+  <meta charset="utf-8">
+  <title>Dodaj obowiązkowy atrybut dla podanej kategorii</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</head>
+<body>
+<?php
+  include ('navbar.php');
+  ?>
+  
+<div class="container-fluid">
+<h3 class="text-white text-center mt-5">Zdefiniuj zestaw obowiązkowych atrybutów dla podanej kategorii zasobów</h3>
+
+<div class="text-center">
+<form method="post" action="" class="form-group">
+
+<?php
+require "db_functions.php";
+open_database();
+$attribute = read_table("attributes.attributes");
+$category = read_table("categories.categories");
+close_database();
+?>
+<div class="container">
+  <div class="row mt-5">
+    <div class="col-md-4">
+<label class="text-white mt-5">
+Kategoria: 
+</label>
+</div>
+<div class="col-md-4"></div>
+<div class="col-md-4">
+<select name="CategoryID" class="form-control mt-5">
+<?php
+foreach($category as $row_number => $row){
+?>
+<option value="<?=$row['cat_id'];?>"><?=$row['cat_name_pl'];?></option>
+<?php
+}
+?>
+</select>
+</div>
+</div>
+</div>
+
+
+<div class="container">
+  <div class="row mt-5">
+    <div class="col-md-4">
+<label class="text-white mt-5">
+Obowiązkowy atrybut dla powyższej kategorii: 
+</label>
+</div>
+<div class="col-md-4"></div>
+<div class="col-md-4">
+<select name="AttributeID" class="form-control mt-5">
+<?php
+foreach($attribute as $row_number => $row){
+?>
+<option value="<?=$row['attribute_id'];?>"><?=$row['attribute_name_pl'];?></option>
+<?php
+}
+?>
+</select>
+</div>
+</div>
+</div>
+
+<div class="container">
+  <div class="row mt-5">
+    <div class="col-md-12">
+<input type="submit" value="Zapisz" class="btn btn-block btn-secondary mt-5">
+</div>
+</div>
+</div>
+</form>
+
+
+<?php
+
+if($_POST)
+{
+  $cat_id = $_POST["CategoryID"];
+  $attribute_id = $_POST["AttributeID"];
+
+  open_database();
+  $result = write_mandatory_attribute($cat_id, $attribute_id);
+  close_database();	
+
+  if (!$result)
+    echo '<br><hr>Nie mogę zapisać obowiązkowego atrybutu!';
+  else
+    echo '<br><hr>Obowiązkowy atrybut zapisany!';
+}
+?>
+</div>
+</div>
+</body>
+</html>
---
+++ poprzednie_wersje+logowanie/inserty/db_units_addOtherUnit.php
@@ -0,0 +1,157 @@
+﻿<!DOCTYPE html>
+<head>
+  <meta charset="utf-8">
+  <title>Dodaj jednostkę dodatkową</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+  </script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+    integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+  </script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+    integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+  </script>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<?php
+  include ('navbar.php');
+  ?>
+<h3 class="text-white text-center mt-3">Dodaj jednostkę dodatkową</h3>
+
+
+<form method="post" action="">
+
+<?php
+require "db_functions.php";
+open_database();
+$quantities = read_table("units.quantities");
+close_database();
+?>
+
+
+<div class="container">
+    <div class="row mt-5">
+   
+      <div class="col-md-4">
+        <label class="text-white ">Jednostka</label>
+      </div>
+      <div class="col-md-4"></div>
+      <div class="col-md-4">
+        <input name="Unit" type="text" class="form-control"/>
+          
+      </div>
+      
+    </div>
+</div>
+
+
+<div class="container">
+    <div class="row mt-5">
+   
+      <div class="col-md-4">
+        <label class="text-white ">Pełna nazwa jednostki PL</label>
+      </div>
+      <div class="col-md-4"></div>
+      <div class="col-md-4">
+        <input name="UnitNamePL" type="text" class="form-control"/>
+          
+      </div>
+      
+    </div>
+</div>
+
+<div class="container">
+    <div class="row mt-5">
+   
+      <div class="col-md-4">
+        <label class="text-white ">Pełna nazwa jednostki ENG</label>
+      </div>
+      <div class="col-md-4"></div>
+      <div class="col-md-4">
+        <input name="UnitNameENG" type="text" class="form-control"/>
+          
+      </div>
+      
+    </div>
+</div>
+
+
+<div class="container">
+    <div class="row mt-5">
+   
+      <div class="col-md-4">
+        <label class="text-white ">Stosunek do jednostki podstawowej</label>
+      </div>
+      <div class="col-md-4"></div>
+      <div class="col-md-4">
+        <input name="Ratio" type="text" class="form-control"/>
+          
+      </div>
+      
+    </div>
+</div>
+
+
+<div class="container">
+  <div class="row mt-5">
+    <div class="col-md-4">
+<label>
+Wielkość fizyczna: 
+</label>
+</div>
+<div class="col-md-4"></div>
+<div class="col-md-4">
+<select name="QuantityID" class="form-control">
+<?php
+foreach($quantities as $row_number => $row){
+?>
+<option value="<?=$row['quantity_id'];?>"><?=$row['quantity_name_pl'];?></option>
+<?php
+}
+?>
+</select>
+</div>
+</div>
+</div>
+
+
+<div class="container">
+    <div class="row">
+      <div class="col-md-12">
+  <input type="submit" value="Zapisz" class="btn btn-block btn-secondary mt-5">
+  </div>
+  </div>
+  </div>
+            
+</form>
+
+
+<?php
+
+if($_POST)
+{
+  $unit               = $_POST["Unit"];
+  $unit_full_name_pl  = $_POST["UnitNamePL"];
+  $unit_full_name_eng = $_POST["UnitNameENG"];
+  $ratio              = $_POST["Ratio"];
+  $quantity_id        = $_POST["QuantityID"];
+
+  open_database();
+  $result = write_other_unit($unit, $unit_full_name_pl, $unit_full_name_eng, $ratio, $quantity_id);
+  close_database();	
+
+  if (!$result)
+    echo '<br><hr>Nie mogę zapisać jednostki!';
+  else
+    echo '<br><hr>Jednostka zapisana!';
+}
+?>
+
+		
+</body>
+</html>
