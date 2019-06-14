179560bf4a59ba46ad831505c0c0da878fd3b1ad
ProjektGrupowyWFIIS <49493484+ProjektGrupowyWFIIS@users.noreply.github.com>
2019-06-14 21:07:57 +0200

    Add files via upload

--- inserty_updaty_selecty_razem/db_units_showUnit.php
+++ inserty_updaty_selecty_razem/db_units_showUnit.php
@@ -24,8 +24,8 @@ include("navbar.php");
 
 <div class="container">
 <div class="row mt-5">
-<div class="col-md-2"></div>
-<div class="col-md-8">
+/*<div class="col-md-2"></div>
+<div class="col-md-8">*/
 <?php
 
 require "db_functions.php";
